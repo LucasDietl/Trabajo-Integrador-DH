007af4e674242497da80987035cfd31ce29f3359
Lucas <dietllucas@hotmail.com>
2017-09-08 13:04:15 -0300

    se agrago el navbar mediante require_once y se le puso un color copado

--- html/home.php
+++ html/home.php
@@ -15,23 +15,7 @@
   </head>
   <body>
 <!-- NOTE: Navbar start -->
-    <nav class="navbar navbar-default navbar-fixed-top navbarcolor" >
-      <div class="container" role="navigation">
-        <a class="navbar-brand logoname" href="index.html">THE<br>BLONDIE</a>
-        <ul class="nav navbar-nav">
-          <li><a href="index.html">Home</a></li>
-          <li><a href="signup.html">Sign Up</a></li>
-          <li><a href="signin.html">Log In</a></li>
-          <li><a href="faq.html">FAQ</a></li>
-        </ul>
-        <form class="navbar-form navbar-right" role="search">
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Search">
-          </div>
-          <button type="submit" class="btn btn-default">Submit</button>
-        </form>
-      </div>
-    </nav>
+<?php require_once("navbar.php"); ?>
 <!-- NOTE: Navbar end -->
 <!-- NOTE: introcuccion start -->
 <div class="" style="display: flex;">
---
+++ html/navbar.php
@@ -0,0 +1,19 @@
+
+
+<nav class="navbar navbar-default navbar-fixed-top navbarcolor" >
+  <div class="container" role="navigation">
+    <a class="navbar-brand logoname" href="index.html">THE<br>BLONDIE</a>
+    <ul class="nav navbar-nav">
+      <li><a href="home.php">Home</a></li>
+      <li><a href="signup.php">Sign Up</a></li>
+      <li><a href="signin.php">Log In</a></li>
+      <li><a href="faq.php">FAQ</a></li>
+    </ul>
+    <form class="navbar-form navbar-right" role="search">
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="Search">
+      </div>
+      <button type="submit" class="btn btn-default">Submit</button>
+    </form>
+  </div>
+</nav>
