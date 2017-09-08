9e4bcda24c73d4fcb019b418daa19f803d0b0fc9
Lucas <dietllucas@hotmail.com>
2017-09-08 13:13:50 -0300

    se puso color al footer y se lo agrega al home mediante require once

---
+++ html/footer.php
@@ -0,0 +1,46 @@
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-4">
+        <h5>Get started</h5>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
+        </ul>
+      </div>
+      <div class="col-sm-4">
+        <h5>Get started</h5>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
+        </ul>
+      </div>
+      <div class="col-sm-4">
+        <h5>Get started</h5>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <iframe class="col-sm-12"id="map-container" frameborder="0" style="border:0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31286.51358666379!2d-58.55486426055731!3d-34.487932968802525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb0566329b7d5%3A0xbf5c5047b087981b!2sSan+Isidro+Partido%2C+Buenos+Aires+Province!5e0!3m2!1sen!2sar!4v1504468145306">
+      </iframe>
+    </div>
+    <div class="social-networks">
+      <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+      <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+      <a href="#" class="google"><i class="fa fa-google-plus"></i></a>
+    </div>
+  </div>
+  <div class="copyright">
+    <div class="container">
+      <div class="col-md-6">
+        <p>© 2017 - Todos los derechos reservados</p>
+      </div>
+    </div>
+  </div>
+</footer>
