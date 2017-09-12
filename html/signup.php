3ce6990eebdbdc55435ec5b9b956555c6c78f67d
Agustin Biagi <agustin.biagi@gmail.com>
2017-09-12 20:34:07 -0300

    Igual

--- html/signup.php
+++ html/signup.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="" href="css/style.css">
     <link rel="icon" type="image/png" href="images/favicon.png">
     <meta name="viewport"content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/css?family=Sedgwick+Ave|Sunshiney" rel="stylesheet">
