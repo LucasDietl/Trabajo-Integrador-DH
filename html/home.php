da055713f46db6e8e5995c7ec591179c3469cfec
Lucas <dietllucas@hotmail.com>
2017-09-18 13:14:05 -0300

    se agregaron mas productos al home

--- html/home.php
+++ html/home.php
@@ -133,6 +133,99 @@
               </div>
             </div>
           </div>
+     </div>
+     <div class="container">
+          <div class="row muestra">
+            <div class="col-xs-12 col-sm-4 col-md-2 growup">
+              <div class="panel panel-danger">
+                <div class="panel-heading">Tempodara</div>
+                <div class="panel-body"><img src="images/zapatostemporada.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+                <div class="panel-footer">2x1 en la compra de cualquier par de zapatos</div>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-2 growup ">
+              <div class="panel panel-danger">
+                <div class="panel-heading">Promo!!</div>
+                <div class="panel-body"><img src="images/zapatonegro.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+                <div class="panel-footer">2x1 en la compra de cualquier par de zapatos</div>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-2 growup ">
+              <div class="panel panel-danger">
+                <div class="panel-heading">Promo!!</div>
+                <div class="panel-body"><img src="images/piernasyzapatos.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+                <div class="panel-footer">2x1 en la compra de cualquier par de zapatos</div>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-2 growup ">
+              <div class="panel panel-danger">
+                <div class="panel-heading">Promo!!</div>
+                <div class="panel-body"><img src="images/pink.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+                <div class="panel-footer">2x1 en la compra de cualquier par de zapatos</div>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-2 growup ">
+              <div class="panel panel-danger">
+                <div class="panel-heading">Promo!!</div>
+                <div class="panel-body"><img src="images/blackwhite.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+                <div class="panel-footer">2x1 en la compra de cualquier par de zapatos</div>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-2 growup ">
+              <div class="panel panel-danger">
+                <div class="panel-heading">Promo!!</div>
+                <div class="panel-body"><img src="images/zapatosvarios.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+                <div class="panel-footer">2x1 en la compra de cualquier par de zapatos</div>
+              </div>
+            </div>
+          </div>
+     </div>
+
+     <div class="container">
+          <div class="row muestra">
+            <div class="col-xs-12 col-sm-4 col-md-2 growup  ">
+              <div class="panel panel-danger">
+                <div class="panel-heading">Promo!!</div>
+                <div class="panel-body"><img src="images/piernasyzapatos.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+                <div class="panel-footer">2x1 en la compra de cualquier par de zapatos</div>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-2 growup ">
+              <div class="panel panel-danger">
+                <div class="panel-heading">Promo!!</div>
+                <div class="panel-body"><img src="images/skate.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+                <div class="panel-footer">2x1 en la compra de cualquier par de zapatos</div>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-2 growup ">
+              <div class="panel panel-danger">
+                <div class="panel-heading">Promo!!</div>
+                <div class="panel-body"><img src="images/pink.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+                <div class="panel-footer">2x1 en la compra de cualquier par de zapatos</div>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-2 growup ">
+              <div class="panel panel-danger">
+                <div class="panel-heading">Promo!!</div>
+                <div class="panel-body"><img src="images/zapatostemporada.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+                <div class="panel-footer">2x1 en la compra de cualquier par de zapatos</div>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-2 growup ">
+              <div class="panel panel-danger">
+                <div class="panel-heading">Promo!!</div>
+                <div class="panel-body"><img src="images/skate.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+                <div class="panel-footer">2x1 en la compra de cualquier par de zapatos</div>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-2 growup ">
+              <div class="panel panel-danger">
+                <div class="panel-heading">Promo!!</div>
+                <div class="panel-body"><img src="images/zapatonegro.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+                <div class="panel-footer">2x1 en la compra de cualquier par de zapatos</div>
+              </div>
+            </div>
+          </div>
      </div><br><br>
      <!-- NOTE: contenido de os productos destacados end -->
      <!-- NOTE: Boton de subir solo para mobile Start -->
